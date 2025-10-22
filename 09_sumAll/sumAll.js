56f7bc782a65ef668e6ff844d22ea2ae8d68df0f
SupremelySalty <102546017+SupremelySalty@users.noreply.github.com>
2025-10-22 17:11:31 +0700

    Complete exercise sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    // If negative numbers return "ERROR"
+    // If non-integer numbers return "ERROR"
+    // If not number return "ERROR"
+    // Find the larger number between a and b
+    // For every integer between a and b add to variable sum
+    if (a < 0 || b < 0) return "ERROR";
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+    if (isNaN(a) || isNaN(b)) return "ERROR";
+    if (b < a) [a, b] = [b, a];
+    let sum = 0
+    for (i = a; i <= b; i++) {
+        sum += i;
+    }
+    return sum
 };
 
 // Do not edit below this line
